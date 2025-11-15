57bd9c8fe0eb8b2608b3caf2e48efd01497ebc65
khanzainabcs252618-ship-it <khanzainabcs252618@gmail.com>
2025-11-15 10:28:16 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This is a single line comment
+    cout << "Hello World! \n";
+
+    cout << "Another single line comment \n"; // This is a single line comment
+
+    /* This is a multi
+       line comment */
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
